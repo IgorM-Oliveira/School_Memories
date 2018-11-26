3dc14f2308feab7020df3108bdf41764310a1113
Igor Matos <41808818+ProgramaTop@users.noreply.github.com>
2018-11-25 22:04:45 -0300

    Add files via upload

---
+++ Local.js
@@ -0,0 +1,13 @@
+class Local{
+	constructor(nome_loc, cidade, rua, bairro, num, comit)
+	{
+		this.nome_loc = nome_loc;
+		this.cidade = cidade;
+		this.rua = rua;
+		this.bairro = bairro;
+		this.num = num;
+		this.comit = comit;
+	}
+}
+
+/*Este é um local para a organização das informações*/
---
+++ Top.js
@@ -0,0 +1,38 @@
+class Top{
+	constructor()
+		{
+		if(localStorage.getItem('Local'))
+		{
+			this.lugar = JSON.parse(localStorage.getItem('Local'));
+		}		
+		else
+		{
+			this.lugar = [];
+		} 
+	}
+	inserir(tudo)
+	{
+		this.lugar.push(tudo);
+		localStorage.setItem("Local",JSON.stringify(this.lugar));
+	}
+	listar()
+	{
+		return this.lugar;
+	}
+	buscar(nome)
+	{
+		for(var i=0;i<this.lugar.length;i++)
+		{
+			if(this.lugar[i].nome_loc == nome)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+
+/*Todo isso serve para a construção, inserção, listagem e busca das informaçoes do local*/
